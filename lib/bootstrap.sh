d0ea92546db1555c5203af8392df9c788ed4fabe
Christian Winther <jippignu@gmail.com>
2022-09-11 01:26:12 +0200

    builds
    Signed-off-by: Christian Winther <jippignu@gmail.com>

--- lib/bootstrap.sh
+++ lib/bootstrap.sh
@@ -70,7 +70,6 @@ function docker_args_append_tag_flags() {
 function docker_args_append_build_flags() {
     DOCKER_ARGS+=" --pull"
     DOCKER_ARGS+=" --push"
-    DOCKER_ARGS+=" --load"
     DOCKER_ARGS+=" --builder ${DOCKER_BUILDX_NAME}"
     DOCKER_ARGS+=" --platform linux/amd64,linux/arm64,linux/armhf"
     DOCKER_ARGS+=" --cache-to   type=local,dest=${DOCKER_CACHE_FOLDER}"
