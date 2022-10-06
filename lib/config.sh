705fe1813eb37a5269d2db97e5aeac4410743a16
Christian Winther <jippignu@gmail.com>
2022-10-06 20:42:48 +0200

    update ecr repo name
    Signed-off-by: Christian Winther <jippignu@gmail.com>

--- lib/config.sh
+++ lib/config.sh
@@ -8,11 +8,11 @@ require_main
 
 DOCKER_CACHE_FOLDER=${DOCKER_CACHE_FOLDER:-/tmp/ser2net-build-cache}
 DOCKER_BUILDX_NAME=${DOCKER_BUILDX_NAME:-ser2net-builder}
-PUBLIC_ECR_REGISTRY=${PUBLIC_ECR_REGISTRY:-i2s8u4z7}
+PUBLIC_ECR_REGISTRY=${PUBLIC_ECR_REGISTRY:-jippi}
 
 # Repository names
 REPO_NAME_GITHUB=${REPO_NAME_GITHUB:-jippi/docker-ser2net}
-REPO_NAME_ECR=${REPO_NAME_ECR:-i2s8u4z7/ser2net}
+REPO_NAME_ECR=${REPO_NAME_ECR:-jippi/ser2net}
 REPO_NAME_DOCKER_HUB=${REPO_NAME_DOCKER_HUB:-jippi/ser2net}
 
 # List of releases to skip
